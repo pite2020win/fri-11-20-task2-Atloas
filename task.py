f488d5e1a148f53d8d8abefd93d3ad9801b9676a
miter010 <miter010@gmail.com>
2020-10-30 11:47:55 +0000

    everything

--- task.py
+++ task.py
@@ -28,3 +28,76 @@
 #
 #Delete these comments before commit!
 #Good luck.
+
+class SubjectGrades:
+  def __init__(self, name):
+    self.name = name
+    self.grades = {}
+
+  def add_grade(self, weight, grade):
+    if weight in self.grades:
+      self.grades[weight].append(grade)
+    else:
+      self.grades[weight] = [grade]
+
+class Student:
+  def __init__(self, name, surname, subjects):
+    self.name = name
+    self.surname = surname
+    self.attendance = {}
+    self.subjectGrades = {}
+    for subject in subjects:
+      self.subjectGrades[subject] = SubjectGrades(subject)
+
+  def __str__(self):
+    return "Student {name} {surname}".format(name=self.name, surname=self.surname)
+
+class HClass:
+  def __init__(self, year, letter, subjects):
+    self.students = []
+    self.year = year
+    self.letter = letter
+    self.name = "{year}{letter}".format(year=year, letter=letter)
+    self.subjects = subjects
+
+  def add_student(self, name, surname):
+    self.students.append(Student(name, surname, self.subjects))
+  
+  def add_students(self, studentList):
+    for student in studentList:
+      self.add_student(student.get("name"), student.get("surname"))
+
+  def __str__(self):
+    to_print = "Class {name}\n".format(name=self.name)
+    for student in self.students:
+      to_print += "-{student}\n".format(student=student)
+    return to_print
+
+class School:
+  def __init__(self, name):
+    self.name = name
+    self.hclasses = []
+
+  def add_class(self, name):
+    self.hclasses.append(HClass(name))
+
+  def get_hclass_by_name(hclass_name):
+    for hclass in hclasses:
+      if hclass.name == hclass_name:
+        return hclass
+
+if __name__ == "__main__":
+  c = HClass(1, "A", ["Maths", "Physics"])
+  c.add_student("A", "B")
+  c.add_student("C", "D")
+  studentList = []
+  student1 = {}
+  student1["name"] = "E"
+  student1["surname"] = "F"
+  student2 = {}
+  student2["name"] = "G"
+  student2["surname"] = "H"
+  studentList.append(student1)
+  studentList.append(student2)
+  c.add_students(studentList)
+  print(str(c))
